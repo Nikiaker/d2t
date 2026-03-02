62c96f31b632a71cec8c77ee5a7b401a1411dbc2
Student 151915 <inf151915@slurm.cs.put.poznan.pl>
2026-03-02 22:43:29 +0100

    new batch

--- problems/triples_to_text/batch_template.sh
+++ problems/triples_to_text/batch_template.sh
@@ -11,24 +11,22 @@ CUDA_VISIBLE_DEVICES=0,1 \
 conda run -n vllm-env vllm serve \
 	RedHatAI/Llama-3.3-70B-Instruct-FP8-dynamic \
     --port 2993 \
-    --api-key AiIsMyLife25 \
-    --gpu-memory-utilization 0.99 \
-    --kv-cache-memory=8000000000 \
+    --gpu-memory-utilization 0.9 \
     --max-model-len 16000 \
+    --tensor-parallel-size 2 \
     > "$SERVER_LOG1" 2>&1 &
 SERVER_PID1=$!
 
 conda run -n openevolve-env python ~/d2t/.conda/test-response.py --port 2993
 
 CUDA_VISIBLE_DEVICES=2,3 \
+SERVER_LOG="/home/inf151915/vllm-server.log"
 conda run -n vllm-env vllm serve \
 	Qwen/Qwen3-Next-80B-A3B-Instruct-FP8 \
     --port 2994 \
-    --api-key AiIsMyLife25 \
-    --gpu-memory-utilization 0.99 \
-    --kv-cache-memory=926329180 \
+    --gpu-memory-utilization 0.9 \
     --max-model-len 16000 \
-    --enforce-eager \
+    --tensor-parallel-size 2 \
     > "$SERVER_LOG2" 2>&1 &
 SERVER_PID2=$!
 
