f893d659ea084b53cad43231563067edd88ec013
nikiaker <md.macek@gmail.com>
2026-03-23 11:46:39 +0100

    docker change

--- .docker/run_vllm.sh
+++ .docker/run_vllm.sh
@@ -2,11 +2,11 @@ docker run --device nvidia.com/gpu=all \
 	--name vllm_lmcache_container \
 	-v ~/.cache/huggingface:/root/.cache/huggingface \
  	--env-file .env \
-	-p 8000:8000 \
+	-p 8001:8000 \
 	--ipc=host \
 	vllm_lmcache \
-	--model RedHatAI/Meta-Llama-3.1-8B-Instruct-FP8 \
-	--max-model-len 12000 \
+	--model cyankiwi/Qwen3.5-9B-AWQ-4bit \
+	--gpu-memory-utilization 0.9 \
 	--enforce-eager \
 	--kv-transfer-config \
     '{"kv_connector":"LMCacheConnectorV1",
