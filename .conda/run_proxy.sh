5623485b2cf9d59beedc1e3f6e88fe7ea63f3311
Student 151915 <inf151915@slurm.cs.put.poznan.pl>
2026-03-29 16:19:41 +0200

    move files

--- .conda/run_proxy.sh
+++ .conda/run_proxy.sh
@@ -3,4 +3,4 @@ export HOSTED_VLLM_API_KEY="your-api-key"
 
 litellm --config ~/d2t/.conda/proxy_config.yaml
 
-python batch_wrapper_server.py --upstream-base-url http://localhost:8000 --port 8010 --storage-dir .batch_wrapper_data
+python ~/d2t/tripler/batch_wrapper_server.py --upstream-base-url http://localhost:2993 --port 2992 --storage-dir ~/.batch_wrapper_data
