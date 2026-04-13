fdf77d601364bc3626b7651f8620458860845b0d
nikiaker <md.macek@gmail.com>
2026-04-13 12:09:58 +0200

    new docker

--- .docker/run_vllm.sh
+++ .docker/run_vllm.sh
@@ -12,3 +12,13 @@ docker run --device nvidia.com/gpu=all \
     '{"kv_connector":"LMCacheConnectorV1",
       "kv_role":"kv_both"
     }'
+
+
+docker run --device nvidia.com/gpu=all \
+        --name vllm_container \
+        -v ~/.cache/huggingface:/root/.cache/huggingface \
+        -p 8001:8000 \
+        --ipc=host \
+        vllm/vllm-openai:latest \
+        google/gemma-3-270m-it \
+        --enforce-eager
