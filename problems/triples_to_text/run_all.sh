e5e40195a0730056d883046bd2b6bb712b47a6fb
Student 151915 <inf151915@slurm.cs.put.poznan.pl>
2026-04-21 19:04:22 +0200

    results skipped

--- problems/triples_to_text/run_all.sh
+++ problems/triples_to_text/run_all.sh
@@ -1,19 +1,9 @@
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/Airport_output/Airport.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/Artist_output/Artist.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/Astronaut_output/Astronaut.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/Athlete_output/Athlete.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/Building_output/Building.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/CelestialBody_output/CelestialBody.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/City_output/City.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/ComicsCharacter_output/ComicsCharacter.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/Company_output/Company.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/Food_output/Food.sh
 sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/MeanOfTransportation_output/MeanOfTransportation.sh
-sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/Monument_output/Monument.sh
 sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/Politician_output/Politician.sh
 sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/SportsTeam_output/SportsTeam.sh
 sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/University_output/University.sh
 sbatch ~/d2t/problems/triples_to_text/outputs/1_sm1ibenp/WrittenWork_output/WrittenWork.sh
+
 sbatch ~/d2t/problems/triples_to_text/outputs/2_sm1ibeyp/Airport_output/Airport.sh
 sbatch ~/d2t/problems/triples_to_text/outputs/2_sm1ibeyp/Artist_output/Artist.sh
 sbatch ~/d2t/problems/triples_to_text/outputs/2_sm1ibeyp/Astronaut_output/Astronaut.sh
@@ -62,3 +52,64 @@ sbatch ~/d2t/problems/triples_to_text/outputs/4_sm2ifeyp/Politician_output/Polit
 sbatch ~/d2t/problems/triples_to_text/outputs/4_sm2ifeyp/SportsTeam_output/SportsTeam.sh
 sbatch ~/d2t/problems/triples_to_text/outputs/4_sm2ifeyp/University_output/University.sh
 sbatch ~/d2t/problems/triples_to_text/outputs/4_sm2ifeyp/WrittenWork_output/WrittenWork.sh
+
+
+Submitted batch job 906781
+Submitted batch job 906782
+Submitted batch job 906783
+Submitted batch job 906784
+Submitted batch job 906785
+Submitted batch job 906786
+Submitted batch job 906787
+Submitted batch job 906788
+Submitted batch job 906789
+Submitted batch job 906790
+Submitted batch job 906791
+Submitted batch job 906792
+Submitted batch job 906793
+Submitted batch job 906794
+Submitted batch job 906795
+Submitted batch job 906796
+Submitted batch job 906797
+Submitted batch job 906798
+Submitted batch job 906799
+Submitted batch job 906800
+Submitted batch job 906801
+Submitted batch job 906802
+Submitted batch job 906803
+Submitted batch job 906804
+Submitted batch job 906805
+Submitted batch job 906806
+Submitted batch job 906807
+Submitted batch job 906808
+Submitted batch job 906809
+Submitted batch job 906810
+Submitted batch job 906811
+Submitted batch job 906812
+Submitted batch job 906813
+Submitted batch job 906814
+Submitted batch job 906815
+Submitted batch job 906816
+Submitted batch job 906817
+Submitted batch job 906818
+Submitted batch job 906819
+Submitted batch job 906820
+Submitted batch job 906821
+Submitted batch job 906822
+Submitted batch job 906823
+Submitted batch job 906824
+Submitted batch job 906825
+Submitted batch job 906826
+Submitted batch job 906827
+Submitted batch job 906828
+Submitted batch job 906829
+Submitted batch job 906830
+Submitted batch job 906831
+Submitted batch job 906832
+Submitted batch job 906833
+Submitted batch job 906834
+Submitted batch job 906835
+Submitted batch job 906836
+Submitted batch job 906837
+Submitted batch job 906838
+Submitted batch job 906839
