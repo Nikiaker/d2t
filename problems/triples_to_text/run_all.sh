3ade148d56cc0d1cab47350ab94686ed7f5ab737
Dominik Maćkowiak <plgnikiaker@login01.athena.cyfronet.pl>
2026-08-15 15:16:56 +0200

    new tests

--- problems/triples_to_text/run_all.sh
+++ problems/triples_to_text/run_all.sh
@@ -1,8 +1,8 @@
-sbatch $D2TPATH/problems/triples_to_text/outputs/5_1k50b30m/WrittenWork_output/WrittenWork.sh
-sbatch $D2TPATH/problems/triples_to_text/outputs/5_1k50b30m/Food_output/Food.sh
-sbatch $D2TPATH/problems/triples_to_text/outputs/5_1krand30m1p/WrittenWork_output/WrittenWork.sh
+sbatch $D2TPATH/problems/triples_to_text/outputs/5_1k30m1p1themis/Building_output/Building.sh
+sbatch $D2TPATH/problems/triples_to_text/outputs/5_1k30m1p1themis/Food_output/Food.sh
+sbatch $D2TPATH/problems/triples_to_text/outputs/5_1k30m1p1themis/WrittenWork_output/WrittenWork.sh
+sbatch $D2TPATH/problems/triples_to_text/outputs/5_1k70mllama4p1themis/Building_output/Building.sh
+sbatch $D2TPATH/problems/triples_to_text/outputs/5_1k70mllama4p1themis/Food_output/Food.sh
+sbatch $D2TPATH/problems/triples_to_text/outputs/5_1k70mllama4p1themis/WrittenWork_output/WrittenWork.sh
+
 sbatch $D2TPATH/problems/triples_to_text/outputs/5_1krand30m1p/Food_output/Food.sh
-sbatch $D2TPATH/problems/triples_to_text/outputs/5_1krand30m4p/WrittenWork_output/WrittenWork.sh
-sbatch $D2TPATH/problems/triples_to_text/outputs/5_1krand30m4p/Food_output/Food.sh
-sbatch $D2TPATH/problems/triples_to_text/outputs/5_1krand30m4p1themis/WrittenWork_output/WrittenWork.sh
-sbatch $D2TPATH/problems/triples_to_text/outputs/5_1krand30m4p1themis/Food_output/Food.sh
