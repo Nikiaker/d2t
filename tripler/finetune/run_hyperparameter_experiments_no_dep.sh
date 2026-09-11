4a132e287e448292a020b73dbac0cfe4803cd15f
Student 151915 <inf151915@slurm.cs.put.poznan.pl>
2026-09-11 15:28:19 +0200

    owid only for ft

--- tripler/finetune/run_hyperparameter_experiments_no_dep.sh
+++ tripler/finetune/run_hyperparameter_experiments_no_dep.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 EXPERIMENTS=(baseline-1epoch)
-DOMAINS=(gsmarena openweather owid wikidata)
+DOMAINS=(owid)
 
 for experiment in "${EXPERIMENTS[@]}"; do
     case "$experiment" in
